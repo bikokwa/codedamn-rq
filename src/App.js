d5f30253f3644214d3093832afd263dc44936b29
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-30 10:51:02 +0300

    hello world

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useQuery } from "react-query";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const { data, error, isLoading, isError, isSuccess, isIdle } = useQuery(
+    "hello-world",
+    () => {
+      return new Promise((resolve) => setTimeout(resolve, 1000));
+    }
   );
+
+  console.log({ data, error, isLoading, isError, isSuccess, isIdle });
+
+  return <div className="App"></div>;
 }
 
 export default App;
