a2430b29f62278989c2f1595997b6940154ef6a2
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-31 07:20:52 +0300

    small github app with react query

--- src/App.js
+++ src/App.js
@@ -2,27 +2,42 @@ import React, { useState } from "react";
 import "./App.css";
 import { useQuery } from "react-query";
 
-function Button() {
-  const { data, error } = useQuery("hello-world", () => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(1337), 1000);
-    });
-  });
-
-  return <button>I am a button</button>;
-}
+const fetcher = (repo) => {
+  return fetch(`https://api.github.com/repos/${repo}`).then((res) =>
+    res.json()
+  );
+};
 
 function App() {
-  const [visible, setVisible] = useState(true);
+  const [repoName, setRepoName] = useState("");
 
-  function toggleButton() {
-    setVisible((visible) => !visible);
+  const { isLoading, data } = useQuery(["github-data", repoName], () =>
+    fetcher(repoName)
+  );
+
+  if (isLoading) {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          value={repoName}
+          onChange={(e) => setRepoName(e.target.value)}
+        />
+        <h2>Loading...</h2>
+      </div>
+    );
   }
 
   return (
     <div className="App">
-      {visible && <Button />}
-      <button onClick={toggleButton}>Toggle</button>
+      <input
+        type="text"
+        value={repoName}
+        onChange={(e) => setRepoName(e.target.value)}
+      />
+      <h2>Name: {data.name}</h2>
+      <h2>Desc: {data.description}</h2>
+      <h2>Stars: {data.stargazers_count}</h2>
     </div>
   );
 }
