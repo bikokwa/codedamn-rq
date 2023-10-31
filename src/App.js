0b05e493c41aac03acead534ab61d9229ae39b55
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-31 05:48:50 +0300

    react query states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import React, { useState } from "react";
 import "./App.css";
 import { useQuery } from "react-query";
 
+function Button() {
+  const { data, error } = useQuery("hello-world", () => {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(1337), 1000);
+    });
+  });
+
+  return <button>I am a button</button>;
+}
+
 function App() {
-  const { data, error, isLoading, isError, isSuccess, isIdle } = useQuery(
-    "hello-world",
-    () => {
-      return new Promise((resolve) => setTimeout(resolve, 1000));
-    }
+  const [visible, setVisible] = useState(true);
+
+  function toggleButton() {
+    setVisible((visible) => !visible);
+  }
+
+  return (
+    <div className="App">
+      {visible && <Button />}
+      <button onClick={toggleButton}>Toggle</button>
+    </div>
   );
-
-  console.log({ data, error, isLoading, isError, isSuccess, isIdle });
-
-  return <div className="App"></div>;
 }
 
 export default App;
